640065a93ded68f78439d3439a3ae7115a3b23d4
Yendry Diaz <yendrydiaz149@gmail.com>
2025-08-25 22:28:02 -0600

    Update Factura.java

--- bll/Factura.java
+++ bll/Factura.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Factura {
+
     private String id;
     private Cliente cliente;
     private List<DetalleFactura> detalles;
@@ -16,15 +17,52 @@ public class Factura {
         this.total = 0;
     }
 
-    public String getId() { return id; }
-    public Cliente getCliente() { return cliente; }
-    public List<DetalleFactura> getDetalles() { return detalles; }
-    public double getTotal() { return total; }
+    public Factura() {
+        this.id = null;
+        this.cliente = null;
+        this.detalles = new ArrayList<>();
+        this.total = 0;
+    }
+    
 
+    public String getId() {
+        return id;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public List<DetalleFactura> getDetalles() {
+        return detalles;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public void setDetalles(List<DetalleFactura> detalles) {
+        this.detalles = detalles;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    
     public void agregarDetalle(DetalleFactura detalle) {
         detalles.add(detalle);
         total += detalle.getSubtotal();
     }
+
+    
+    
 }
-
-
