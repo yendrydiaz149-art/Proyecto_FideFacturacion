ca4dd6a336e9c9b108804fff2ad23ab3529c03bc
Yendry Diaz <yendrydiaz149@gmail.com>
2025-08-25 22:27:46 -0600

    Update DetalleFactura.java

--- bll/DetalleFactura.java
+++ bll/DetalleFactura.java
@@ -1,18 +1,52 @@
 package bll;
 
 public class DetalleFactura {
+
     private Producto producto;
     private int cantidad;
     private double subtotal;
+    private double precioUnitario;
 
-    public DetalleFactura(Producto producto, int cantidad) {
+    public DetalleFactura(Producto producto, int cantidad, double precioUnitario) {
         this.producto = producto;
         this.cantidad = cantidad;
         this.subtotal = producto.getPrecio() * cantidad;
+        this.precioUnitario = precioUnitario;
     }
 
-    public Producto getProducto() { return producto; }
-    public int getCantidad() { return cantidad; }
-    public double getSubtotal() { return subtotal; }
+    public DetalleFactura() {
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public void setSubtotal(double subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public double getPrecioUnitario() {
+        return precioUnitario;
+    }
+
+    public void setPrecioUnitario(double precioUnitario) {
+        this.precioUnitario = precioUnitario;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public double getSubtotal() {
+        return subtotal;
+    }
 }
 
