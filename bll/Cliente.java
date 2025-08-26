b76a5a473e5c63ac8c531b11a23d50285d86c775
Yendry Diaz <yendrydiaz149@gmail.com>
2025-08-25 20:39:13 -0600

    Update Cliente.java

--- bll/Cliente.java
+++ bll/Cliente.java
@@ -1,22 +1,25 @@
-package bll;
+package negocio;
 
-import java.io.Serializable;
-
-public class Cliente implements Serializable {
+public class Cliente {
     private String id;
     private String nombre;
     private String telefono;
+    private String correo;
 
-    public Cliente(String id, String nombre, String telefono) {
+    public Cliente(String id, String nombre, String telefono, String correo) {
         this.id = id;
         this.nombre = nombre;
         this.telefono = telefono;
+        this.correo = correo;
     }
 
     public String getId() { return id; }
     public String getNombre() { return nombre; }
     public String getTelefono() { return telefono; }
+    public String getCorreo() { return correo; }
 
     public void setNombre(String nombre) { this.nombre = nombre; }
     public void setTelefono(String telefono) { this.telefono = telefono; }
+    public void setCorreo(String correo) { this.correo = correo; }
 }
+
