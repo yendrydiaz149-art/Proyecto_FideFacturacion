e586328171fe0734ccbbe1b408b5c367a41a4cb2
Yendry Diaz <yendrydiaz149@gmail.com>
2025-08-25 20:41:34 -0600

    Update DetalleFactura.java

--- bll/DetalleFactura.java
+++ bll/DetalleFactura.java
@@ -1,17 +1,18 @@
 package bll;
 
-import java.io.Serializable;
-
-public class DetalleFactura implements Serializable {
+public class DetalleFactura {
     private Producto producto;
     private int cantidad;
+    private double subtotal;
 
     public DetalleFactura(Producto producto, int cantidad) {
         this.producto = producto;
         this.cantidad = cantidad;
+        this.subtotal = producto.getPrecio() * cantidad;
     }
 
     public Producto getProducto() { return producto; }
     public int getCantidad() { return cantidad; }
-    public double getSubtotal() { return producto.calcularPrecioFinal() * cantidad; }
+    public double getSubtotal() { return subtotal; }
 }
+
